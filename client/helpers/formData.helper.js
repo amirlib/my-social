047b7085a24a5e754e5496a56343dc1f384bc9b6
Amir Liberzon <amir.lib@gmail.com>
2021-02-20 15:14:13 +0200

    not appending undefined values to FormData object

--- client/helpers/formData.helper.js
+++ client/helpers/formData.helper.js
@@ -2,7 +2,9 @@ const createFormData = (values) => {
   const data = new FormData();
   const pairs = Object.entries(values);
 
-  pairs.forEach((pair) => data.append(pair[0], pair[1]));
+  pairs.forEach((pair) => {
+    if (pair[1]) data.append(pair[0], pair[1]);
+  });
 
   return data;
 };
