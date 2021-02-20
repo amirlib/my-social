263d8343137c167e475310779fced4227686c718
Amir Liberzon <amir.lib@gmail.com>
2021-02-20 15:20:49 +0200

    added uploading middleware for user patch route

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,17 +1,35 @@
 import express from 'express';
+import multer from 'multer';
+import { multerConfig } from '../config/multer.config';
 import { hasAuthorization, requireLogin } from '../controllers/auth.controller';
 import * as userCtrl from '../controllers/user.controller';
 
 const router = express.Router();
+const upload = multer(multerConfig);
 
 router.route('/api/users')
-  .get(requireLogin, userCtrl.list)
+  .get(
+    requireLogin,
+    userCtrl.list,
+  )
   .post(userCtrl.create);
 
 router.route('/api/users/:userId')
-  .get(requireLogin, userCtrl.read)
-  .patch(requireLogin, hasAuthorization, userCtrl.update)
-  .delete(requireLogin, hasAuthorization, userCtrl.remove);
+  .delete(
+    requireLogin,
+    hasAuthorization,
+    userCtrl.remove,
+  )
+  .get(
+    requireLogin,
+    userCtrl.read,
+  )
+  .patch(
+    requireLogin,
+    hasAuthorization,
+    upload.single('profilePicture'),
+    userCtrl.update,
+  );
 
 router.param('userId', userCtrl.userById);
 
